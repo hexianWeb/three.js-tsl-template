22b43fc4e4aabb57acdec7c42446cccaec465128
hexianWeb <hexianWeb@gmail.com>
2026-05-20 15:50:46 +0800

    feat: 新增 新的静态资源

--- src/sources.js
+++ src/sources.js
@@ -5,18 +5,65 @@
 export default [
   {
     name: 'positionGridTex',
-    type: 'ktx2Texture',
-    path: 'texture/position_grid.ktx2'
+    type: 'texture',
+    path: 'texture/object/position_grid.webp'
   },
   {
     name: 'renderedGridTex',
-    type: 'ktx2Texture',
-    path: 'texture/rendered_grid.ktx2'
+    type: 'texture',
+    path: 'texture/object/rendered_grid.webp'
   },
   {
     name: 'motionVectorGridTex',
-    type: 'ktx2Texture',
-    path: 'texture/motion_vector_grid.ktx2'
+    type: 'texture',
+    path: 'texture/object/motion_vector_grid.webp'
   },
-  // Add more entries (texture, hdrTexture, video, cubeTexture, etc.) as needed
+  // Camera set (PNG) for panel switching
+  {
+    name: 'positionGridTexCamera',
+    type: 'texture',
+    path: 'texture/camera/position_grid.png'
+  },
+  {
+    name: 'renderedGridTexCamera',
+    type: 'texture',
+    path: 'texture/camera/rendered_grid.png'
+  },
+  {
+    name: 'motionVectorGridTexCamera',
+    type: 'texture',
+    path: 'texture/camera/motion_vector_grid.png'
+  },
+  // Gameboy set (4×4 KTX2)
+  {
+    name: 'positionGridTexGameboy',
+    type: 'ktx2Texture',
+    path: 'texture/gameboy/gameboy_position-high.ktx2'
+  },
+  {
+    name: 'renderedGridTexGameboy',
+    type: 'ktx2Texture',
+    path: 'texture/gameboy/gameboy_diffuse-high.ktx2'
+  },
+  {
+    name: 'alphaGridTexGameboy',
+    type: 'ktx2Texture',
+    path: 'texture/gameboy/gameboy_alpha-high.ktx2'
+  },
+  {
+    name: 'motionVectorGridTexGameboy',
+    type: 'ktx2Texture',
+    path: 'texture/gameboy/gameboy_mv-high.ktx2'
+  },
+  // Phone set (4×4 JPG)
+  {
+    name: 'renderedGridTexPhone',
+    type: 'texture',
+    path: 'texture/phone/rendered_grid.jpg'
+  },
+  {
+    name: 'motionVectorGridTexPhone',
+    type: 'texture',
+    path: 'texture/phone/motion_vector_grid.jpg'
+  }
 ]
